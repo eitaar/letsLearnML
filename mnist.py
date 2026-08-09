f8a2e54da9c018760b04dce8b3a06dbb41c4a39a
eitaar <154091730+eitaar@users.noreply.github.com>
2026-08-09 20:59:12 +0900

    fix

--- mnist.py
+++ mnist.py
@@ -1,50 +1,19 @@
 import os
 import sys
 
-sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
-import pickle
-
 import numpy as np
 
+sys.path.append(os.pardir)
 from dataset.mnist import load_mnist
-from letslearnml.activations import sigmoid, softmax
 
+(x_train, t_train), (x_test, t_test) = load_mnist(one_hot_label=True, normalize=True)
 
-def get_data():
-    (x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=False)
-    return x_test, t_test
+print(x_train.shape)
+print(t_train.shape)
 
-
-def init_network():
-    with open("sample_weight.pkl", "rb") as f:
-        network = pickle.load(f)
-    return network
-
-
-def predict(network, x):
-    w1, w2, w3 = network["W1"], network["W2"], network["W3"]
-    b1, b2, b3 = network["b1"], network["b2"], network["b3"]
-
-    a1 = np.dot(x, w1) + b1
-    z1 = sigmoid(a1)
-    a2 = np.dot(z1, w2) + b2
-    z2 = sigmoid(a2)
-    a3 = np.dot(z2, w3) + b3
-    y = softmax(a3)
-
-    return y
-
-
-x, t = get_data()
-network = init_network()
-
-batch_size = 100  # バッチの数
-accuracy_cnt = 0
-
-for i in range(0, len(x), batch_size):
-    x_batch = x[i : i + batch_size]
-    y_batch = predict(network, x_batch)
-    p = np.argmax(y_batch, axis=1)
-    accuracy_cnt += np.sum(p == t[i : i + batch_size])
-
-print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
+train_size = x_train.shape[0]
+batch_size = 10
+batch_mask = np.random.choice(train_size, batch_size)
+x_batch = x_train[batch_mask]
+t_batch = t_train[batch_mask]
+print(x_batch)
--- src/letslearnml/loss_functions/cel.py
+++ src/letslearnml/loss_functions/cel.py
@@ -1,6 +1,11 @@
 import numpy as np
+from numpy.typing import NDArray
 
 
-def cel(y, t):
+def cel(y: NDArray, t: NDArray) -> NDArray:
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+    batch_size = y.shape[0]
     delta = 1e-7
-    return -np.sum(t * np.log(y + delta))
+    return -np.sum(t * np.log(y + 1e-7))
