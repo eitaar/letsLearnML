90a498379c8170af159ef70323ee31553bfc210a
eitaar <154091730+eitaar@users.noreply.github.com>
2026-08-11 15:59:17 +0900

    created my first NN that works (very slow)

--- mnist.py
+++ mnist.py
@@ -1,19 +1,44 @@
-import os
-import sys
-
+import matplotlib.pyplot as plt
 import numpy as np
 
-sys.path.append(os.pardir)
 from dataset.mnist import load_mnist
+from TLnet import TwoLayerNet
 
-(x_train, t_train), (x_test, t_test) = load_mnist(one_hot_label=True, normalize=True)
+print("Loading data...")
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
 
-print(x_train.shape)
-print(t_train.shape)
+train_loss_list = []
 
+iters_num = 10
 train_size = x_train.shape[0]
-batch_size = 10
-batch_mask = np.random.choice(train_size, batch_size)
-x_batch = x_train[batch_mask]
-t_batch = t_train[batch_mask]
-print(x_batch)
+batch_size = 100
+learning_rate = 0.1
+
+print("Building model...")
+network = TwoLayerNet(784, 50, 10)
+
+print("Training...")
+count = 0
+for i in range(iters_num):
+    count += 1
+    print(f"{count}/{iters_num}")
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    print("calculating grad...")
+    # calc grad
+    grad = network.numerical_gradient(x_batch, t_batch)
+
+    print("upd param...")
+    # upd param
+    for key in ("W1", "b1", "W2", "b2"):
+        network.params[key] -= learning_rate * grad[key]
+
+    print("calc loss...")
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+print("Done")
+
+plt.plot(train_loss_list)
+plt.show()
