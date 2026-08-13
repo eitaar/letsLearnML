930f25459f5dda0d06e4342bfcab7094808a1b6f
eitaar <154091730+eitaar@users.noreply.github.com>
2026-08-13 23:13:15 +0900

    backproping batchnorm

--- src/letslearnml/batches/batch_norm.py
+++ src/letslearnml/batches/batch_norm.py
@@ -3,10 +3,14 @@ import numpy as np
 
 class BatchNorm:
     def __init__(self):
-        self.x = None
-        self.gamma = None
-        self.beta = None
+        self.x: float
+        self.gamma: float
+        self.beta: float
+        self.x_cap: float
         self.eps = 1e-5
+        self.mu: float
+        self.sigma_squared: float
+        self.sigma: float
 
     def forward(self, x, gamma, beta, eps=1e-5):
         self.x = x
@@ -14,14 +18,21 @@ class BatchNorm:
         self.beta = beta
         self.eps = eps
 
-        batch_mean = np.mean(x, axis=0)
-        sigma_squared = np.mean((x - batch_mean) ** 2, axis=0)
-        sigma = np.sqrt(sigma_squared + self.eps)
+        self.mu = np.mean(x, axis=0)
+        self.sigma_squared = np.mean((x - self.mu) ** 2, axis=0)
+        self.sigma = np.sqrt(self.sigma_squared + self.eps)
 
-        x_cap = (x - batch_mean) / sigma
+        self.x_cap = (x - self.mu) / self.sigma
 
-        out = gamma * x_cap + beta
+        out = gamma * self.x_cap + beta
         return out
 
     def backward(self, dout):
-        pass
+        dgamma = np.sum(self.x_cap * dout, axis=0)
+        dbeta = np.sum(dout, axis=0)
+        dx_cap = dout * self.gamma
+        dsigma_sq = np.sum(dx_cap * (self.x - self.mu), axis=0) * -0.5 * self.sigma**-3
+        dmu = np.sum(-dx_cap / self.sigma, axis=0) + dsigma_sq * np.mean(-2 * (self.x - self.mu), axis=0)
+        dx = dx_cap / self.sigma + dsigma_sq * (2 * (self.x - self.mu) / self.x.shape[0]) + dmu * 1 / self.x.shape[0]
+
+        return dx
